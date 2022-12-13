96f6dcbc81fae9c2aca258c0afaf0da6ff0ff5f6
Makan Kamissoko <makan.kamissoko@hotmail.fr>
2022-12-13 11:58:21 +0100

    test

--- database/migrations/2022_12_13_094624_create_users_table.php
+++ database/migrations/2022_12_13_094624_create_users_table.php
@@ -16,7 +16,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('firstname');
+            $table->string('pseudo');
             $table->string('email')->unique();
+            $table->string('phonenumber');
+            $table->double('notes');
+            $table->string('adresse');
+            $table->enum('role', ['admin', 'user', 'inactive']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
